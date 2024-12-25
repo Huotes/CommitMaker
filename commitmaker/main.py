f03a0296b0cea81170002c4c112ad61c5d48282a
Huotes <aureliodosol@gmail.com>
2024-12-25 19:06:06 -0300

    chore: atualiza dependências e limpa código

--- commitmaker/main.py
+++ commitmaker/main.py
@@ -2,7 +2,6 @@ from .logger import registrar_log
 from .commit import realizar_commit
 from .readme_updater import atualizar_readme
 
-
 def main():
     try:
         # Atualiza o README com uma frase de meme
@@ -12,12 +11,22 @@ def main():
         realizar_commit()
         
         # Registra o sucesso no log
-        registrar_log("Commit realizado com sucesso.")
+        registrar_log(
+            event="main_execution",
+            status="success",
+            details="Commit realizado com sucesso.",
+            extra=None
+        )
         
         print("🚀 Commit realizado com sucesso!")
     except Exception as e:
         # Registra qualquer erro no log
-        registrar_log(f"Erro: {e}")
+        registrar_log(
+            event="main_execution",
+            status="error",
+            details=f"Erro durante a execução principal: {e}",
+            extra=None
+        )
         print(f"❌ Erro: {e}")
 
 if __name__ == "__main__":
